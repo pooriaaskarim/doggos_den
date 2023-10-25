b575ba6d462a0d3d82b3d9571c954b6230bdcc6a
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-10-25 17:26:03 +0330

    Changes in App Theme

--- lib/infrastructure/config/theme/components_theme/app.text_button_theme_data.dart
+++ lib/infrastructure/config/theme/components_theme/app.text_button_theme_data.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../../../utils/app.sizes.dart';
-import '../../../utils/app.utils.dart';
 import '../app.elevations.dart';
 import '../app.fonts.dart';
-import '../app.opacities.dart';
 
 class AppTextButtonThemeData extends TextButtonThemeData {
   AppTextButtonThemeData(final ColorScheme colorScheme)
       : super(
           style: ButtonStyle(
+            backgroundColor:
+                const MaterialStatePropertyAll<Color>(Colors.transparent),
+            foregroundColor:
+                const MaterialStatePropertyAll<Color>(Colors.transparent),
+            overlayColor:
+                const MaterialStatePropertyAll<Color>(Colors.transparent),
+            shadowColor:
+                const MaterialStatePropertyAll<Color>(Colors.transparent),
             padding: const MaterialStatePropertyAll(
               EdgeInsets.symmetric(
                 vertical: AppSizes.points_12,
@@ -20,17 +26,12 @@ class AppTextButtonThemeData extends TextButtonThemeData {
               (final states) {
                 if (states.any(
                   {
+                    MaterialState.hovered,
+                    MaterialState.focused,
                     MaterialState.disabled,
                   }.contains,
                 )) {
                   return AppElevations.level_0;
-                }
-                if (states.any(
-                  {
-                    MaterialState.hovered,
-                  }.contains,
-                )) {
-                  return AppElevations.level_2;
                 }
                 //focused, pressed and enabled states
                 return AppElevations.level_1;
@@ -58,12 +59,6 @@ class AppTextButtonThemeData extends TextButtonThemeData {
                 );
               },
             ),
-            shape: const MaterialStatePropertyAll(
-              ContinuousRectangleBorder(
-                borderRadius:
-                    BorderRadius.all(Radius.circular(AppUtils.borderRadius)),
-              ),
-            ),
             iconColor: MaterialStateProperty.resolveWith<Color?>(
               (final states) {
                 if (states.any(
@@ -77,154 +72,55 @@ class AppTextButtonThemeData extends TextButtonThemeData {
                 return colorScheme.primary;
               },
             ),
+            side:
+                MaterialStateProperty.resolveWith<BorderSide?>((final states) {
+              if (states.any(
+                {
+                  MaterialState.hovered,
+                }.contains,
+              )) {
+                return BorderSide(color: colorScheme.primary, width: 1);
+              }
+              if (states.any(
+                {
+                  MaterialState.pressed,
+                  MaterialState.selected,
+                  MaterialState.dragged,
+                }.contains,
+              )) {
+                return BorderSide(color: colorScheme.primary, width: 4);
+              } else {
+                return null;
+              }
+            }),
+            shape: MaterialStateProperty.resolveWith<OutlinedBorder?>(
+                (final states) {
+              if (states.any(
+                {
+                  MaterialState.hovered,
+                }.contains,
+              )) {
+                return LinearBorder.bottom(
+                  side: BorderSide(color: colorScheme.primary, width: 1),
+                );
+              }
+              if (states.any(
+                {
+                  MaterialState.pressed,
+                  MaterialState.selected,
+                  MaterialState.dragged,
+                }.contains,
+              )) {
+                return LinearBorder.bottom(
+                  side: BorderSide(color: colorScheme.primary, width: 2),
+                );
+              } else {
+                return const ContinuousRectangleBorder(
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(AppSizes.points_0)),
+                );
+              }
+            }),
           ),
         );
-
-  static ButtonStyle surfaceStyle(final ColorScheme colorScheme) =>
-      AppTextButtonThemeData(colorScheme).style!.copyWith(
-            backgroundColor: MaterialStatePropertyAll(colorScheme.surface),
-            elevation: MaterialStateProperty.resolveWith<double?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.disabled,
-                  }.contains,
-                )) {
-                  return AppElevations.level_0;
-                }
-                if (states.any(
-                  {
-                    MaterialState.focused,
-                    MaterialState.hovered,
-                  }.contains,
-                )) {
-                  return AppElevations.level_0;
-                }
-                // pressed and enabled states
-                return AppElevations.level_0;
-              },
-            ),
-            iconColor: MaterialStatePropertyAll(colorScheme.onSurface),
-            textStyle: MaterialStatePropertyAll(AppFonts.textTheme.labelLarge),
-            // shadowColor: MaterialStatePropertyAll(
-            //   colorScheme.shadow,
-            // ),
-          );
-
-  static ButtonStyle elevatedStyle(final ColorScheme colorScheme) =>
-      AppTextButtonThemeData(colorScheme).style!.copyWith(
-            backgroundColor: MaterialStatePropertyAll(colorScheme.background),
-            elevation: MaterialStateProperty.resolveWith<double?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.disabled,
-                  }.contains,
-                )) {
-                  return AppElevations.level_1;
-                }
-                if (states.any(
-                  {
-                    MaterialState.focused,
-                    MaterialState.hovered,
-                  }.contains,
-                )) {
-                  return AppElevations.level_4;
-                }
-                // pressed and enabled states
-                return AppElevations.level_3;
-              },
-            ),
-            iconColor: MaterialStatePropertyAll(colorScheme.onSurface),
-            textStyle: MaterialStatePropertyAll(AppFonts.textTheme.labelLarge),
-            shadowColor: MaterialStatePropertyAll(
-              colorScheme.shadow,
-            ),
-          );
-
-  static ButtonStyle elevatedErrorStyle(final ColorScheme colorScheme) =>
-      AppTextButtonThemeData(colorScheme).style!.copyWith(
-            backgroundColor: MaterialStatePropertyAll(colorScheme.error),
-            elevation: MaterialStateProperty.resolveWith<double?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.disabled,
-                  }.contains,
-                )) {
-                  return AppElevations.level_1;
-                }
-                if (states.any(
-                  {
-                    MaterialState.focused,
-                    MaterialState.hovered,
-                  }.contains,
-                )) {
-                  return AppElevations.level_4;
-                }
-                // pressed and enabled states
-                return AppElevations.level_3;
-              },
-            ),
-            iconColor: MaterialStatePropertyAll(colorScheme.onError),
-            textStyle: MaterialStatePropertyAll(AppFonts.textTheme.labelLarge),
-            shadowColor: MaterialStatePropertyAll(
-              colorScheme.shadow,
-            ),
-          );
-
-  static ButtonStyle tertiaryStyle(final ColorScheme colorScheme) =>
-      AppTextButtonThemeData(colorScheme).style!.copyWith(
-            iconColor: MaterialStateProperty.resolveWith<Color?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.disabled,
-                  }.contains,
-                )) {
-                  return colorScheme.onSurface.withOpacity(38 / 100);
-                }
-                //Any State other than disabled
-                return colorScheme.tertiary;
-              },
-            ),
-            elevation: MaterialStateProperty.resolveWith<double?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.hovered,
-                  }.contains,
-                )) {
-                  return AppElevations.level_2;
-                }
-                //focused, pressed, disabled and enabled states
-                return AppElevations.level_0;
-              },
-            ),
-            backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-              (final states) {
-                if (states.any(
-                  {
-                    MaterialState.disabled,
-                  }.contains,
-                )) {
-                  return colorScheme.onSurface.withOpacity(38 / 100);
-                }
-                if (states.any(
-                  {
-                    MaterialState.pressed,
-                  }.contains,
-                )) {
-                  return colorScheme.tertiary
-                      .withOpacity(AppOpacities.pressStateLayerOpacity);
-                }
-                //Any State other than disabled
-                return null;
-              },
-            ),
-            textStyle: MaterialStatePropertyAll(AppFonts.textTheme.labelLarge),
-            shadowColor: MaterialStatePropertyAll(
-              colorScheme.shadow,
-            ),
-          );
 }

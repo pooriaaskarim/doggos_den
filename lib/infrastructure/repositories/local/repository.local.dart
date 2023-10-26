348432e657dff21a8d7d8d052b91e08967334e7b
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-10-26 10:45:31 +0330

    Local Repository Refactored:     Theme Persistence implemented

--- lib/infrastructure/repositories/local/repository.local.dart
+++ lib/infrastructure/repositories/local/repository.local.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
+
+const _themeModeKey = 'themeMode';
 
 class LocalRepository {
   factory LocalRepository.instance() => _instance;
@@ -25,31 +26,37 @@ class LocalRepository {
       ? Hive.box<M>(_instanceName)
       : await Hive.openBox<M>(
           _instanceName,
-          path:
-              !kIsWeb ? (await getApplicationDocumentsDirectory()).path : null,
+          path: (await getApplicationDocumentsDirectory()).path,
         );
 
   // Implementation
 
-  Future<String?> readTheme() async {
-    final Box<String> db = await _openDb<String>();
-    final user = db.get('theme');
+  Future<ThemeMode> getThemeMode() async {
+    final Box db = await _openDb();
+    final String? themeMode = db.get(_themeModeKey);
     await db.close();
-    return user;
+    switch (themeMode) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+      default:
+        return ThemeMode.system;
+    }
   }
 
-  Future<void> persistTheme(final ThemeData theme) async {
+  Future<void> persistThemeMode(final ThemeMode themeMode) async {
     final Box<String> db = await _openDb<String>();
     await db.put(
-      'theme',
-      theme.brightness == Brightness.light ? 'light' : 'dark',
+      _themeModeKey,
+      themeMode.name,
     );
     await db.close();
   }
 
-  Future<void> clearTheme() async {
-    final Box<String> db = await _openDb<String>();
-    await db.delete('Theme');
+  Future<void> clearThemeMode() async {
+    final Box db = await _openDb();
+    await db.delete(_themeModeKey);
     await db.close();
   }
 }

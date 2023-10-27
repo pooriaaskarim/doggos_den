d9046dc00b51c7baa1265b286b41d15d1177532a
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-10-27 12:49:43 +0330

    AppBloc:     States Enhanced     Events Enhanced     App initialization and data fetching implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app.dart';
 import 'application/app_bloc/app_bloc.dart';
 import 'infrastructure/repositories/local/repository.local.dart';
+import 'infrastructure/repositories/network/doggo.repository.dart';
 
 void main() {
   runApp(
-    BlocProvider(
-      create: (final context) => AppBloc(
-        localRepository: LocalRepository.instance(),
+    RepositoryProvider(
+      create: (final context) => DoggoRepository(),
+      child: BlocProvider(
+        create: (final context) => AppBloc(
+          localRepository: LocalRepository.instance(),
+          doggoRepository: RepositoryProvider.of<DoggoRepository>(context),
+        ),
+        child: const App(),
       ),
-      child: const App(),
     ),
   );
 }
